a78984364f32547120cb0af2874cdb3130889d79
Ariel Gerstein <ariel.gers@gmail.com>
2023-03-21 23:04:02 -0300

    add pixel art mode / scaling

--- client/src/index.ts
+++ client/src/index.ts
@@ -7,10 +7,13 @@ const config: Phaser.Types.Core.GameConfig = {
   physics: {
     default: "arcade",
   },
-  // @TODO: Review how to adapt to changing viewport size
-  width: 500,
-  height: 500,
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  parent: document.getElementById("game")!,
+  width: 400,
+  height: 400,
   scene: GameScene,
+  pixelArt: true,
+  zoom: 2,
 };
 
 new Phaser.Game(config);
