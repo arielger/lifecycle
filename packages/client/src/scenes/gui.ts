d06b0a7fb8690f1a24635bc4b3948ea75d28c057
Ariel Gerstein <ariel.gers@gmail.com>
2023-03-31 18:35:32 -0300

    remove gui dependency to fix deploy

--- packages/client/src/scenes/gui.ts
+++ packages/client/src/scenes/gui.ts
@@ -1,16 +1,16 @@
-import * as dat from "dat.gui";
+// import * as dat from "dat.gui";
 
-export const gameConfig = {
-  lag: 0,
-  // If false it will simulate a client-side only game
-  serverSideProcessing: true,
-  clientSidePrediction: true,
-  serverReconciliation: true,
-};
+// export const gameConfig = {
+//   lag: 0,
+//   // If false it will simulate a client-side only game
+//   serverSideProcessing: true,
+//   clientSidePrediction: true,
+//   serverReconciliation: true,
+// };
 
-const gui = new dat.GUI({ name: "My GUI", width: 400 });
+// const gui = new dat.GUI({ name: "My GUI", width: 400 });
 
-gui.add(gameConfig, "clientSidePrediction").name("Client-side prediction");
-gui.add(gameConfig, "serverReconciliation").name("Server reconciliation");
-gui.add(gameConfig, "serverSideProcessing").name("Server-side processing");
-gui.add(gameConfig, "lag", 0, 500);
+// gui.add(gameConfig, "clientSidePrediction").name("Client-side prediction");
+// gui.add(gameConfig, "serverReconciliation").name("Server reconciliation");
+// gui.add(gameConfig, "serverSideProcessing").name("Server-side processing");
+// gui.add(gameConfig, "lag", 0, 500);
